6ef5d713f8bfd6ae28da3ee4fb08bf6e95659c28
Mike Manger <mike.manger@lighthouseuk.net>
2025-02-03 18:12:05 +0000

    Remove unsuported django code from docs/tests

--- test_app/djqscsv_tests/models.py
+++ test_app/djqscsv_tests/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.utils.translation import ugettext as _
+from django.utils.translation import gettext as _
 from datetime import datetime
 
 SOME_TIME = datetime(2001, 1, 1, 1, 1)
--- test_app/djqscsv_tests/urls.py
+++ test_app/djqscsv_tests/urls.py
@@ -1,6 +1,6 @@
-from django.conf.urls import url
+from django.urls import path
 from djqscsv_tests import views
 
 urlpatterns = (
-    url(r'^$', views.get_csv, name='get_csv'),
+    path('', views.get_csv, name='get_csv'),
 )
